fbf76ae684898f64b67f8899159d38ca51ace245
tiawebdemo <35865533+tiaweb@users.noreply.github.com>
2020-04-30 14:32:26 +0700

    Update ProfilePage.js

--- src/views/ProfilePage/ProfilePage.js
+++ src/views/ProfilePage/ProfilePage.js
@@ -47,7 +47,7 @@ export default function ProfilePage(props) {
     <div>
       <Header
         color="transparent"
-        brand="Material Kit React"
+        brand="Tiaweb React R3"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
